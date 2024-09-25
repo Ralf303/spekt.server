dec05306169cd17624b145a0308572ec05d84043
ralf303 <nikitasuhanov34@gmail.com>
2024-09-25 11:42:14 +0300

    beta map api

--- src/map/map.service.ts
+++ src/map/map.service.ts
@@ -6,33 +6,33 @@ import { Map } from "src/types/map";
 export class MapService {
   constructor(private readonly prisma: PrismaService) {}
 
-  async onModuleInit() {
-    console.log("Модуль подключен, начинаю генерацию базы...");
+  // async onModuleInit() {
+  //   console.log("Модуль подключен, начинаю генерацию базы...");
 
-    for (let i = 0; i < 50000; i++) {
-      const lat = Number((Math.random() * 180 - 90).toFixed(6));
-      const lon = Number((Math.random() * 360 - 180).toFixed(6));
+  //   for (let i = 0; i < 50000; i++) {
+  //     const lat = Number((Math.random() * 180 - 90).toFixed(6));
+  //     const lon = Number((Math.random() * 360 - 180).toFixed(6));
 
-      const map = await this.prisma.map.findFirst({
-        where: { lat: lat, lon: lon },
-      });
+  //     const map = await this.prisma.map.findFirst({
+  //       where: { lat: lat, lon: lon },
+  //     });
 
-      if (map) {
-        continue;
-      }
-      await this.prisma.map.create({
-        data: {
-          name: "test",
-          type: "point",
-          lat,
-          lon,
-        },
-      });
-      console.log("Успешно создано!");
-    }
+  //     if (map) {
+  //       continue;
+  //     }
+  //     await this.prisma.map.create({
+  //       data: {
+  //         name: "test",
+  //         type: "point",
+  //         lat,
+  //         lon,
+  //       },
+  //     });
+  //     console.log("Успешно создано!");
+  //   }
 
-    console.log("Генерация завершена.");
-  }
+  //   console.log("Генерация завершена.");
+  // }
 
   async getAll(
     startLat: number,
